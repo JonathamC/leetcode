41019ea314fc3095fc59c7de17b02e7ccfef7c8e
JonathamC <86174437+JonathamC@users.noreply.github.com>
2022-03-11 15:34:34 -0500

    Update Fibonacci.py

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,23 +1,26 @@
 import locale 
 locale.setlocale(locale.LC_ALL, 'en_US')
 
-
-# used in top down approach 
-F = [-1] * 50 
-
+# Top down is better than bottom up approach
 def top_down_fib(n): 
-    if F[n] < 0: 
-        if (n == 0): 
-            F[n] = 0
-        elif (n == 1): 
-            F[n] = 1
-        else: 
-            F[n] = top_down_fib(n - 1) + top_down_fib(n - 2)
-
-    return F[n]
+    fib = [] 
+    if n == 0 or n == 1: 
+        return n 
+    
+    fib.append(1)
+    fib.append(1)
+    for i in range(2, n): 
+        fib.append(fib[i - 1] + fib[i - 2])
+    return fib[n-1]
 
 def bottom_up_fib(n):
     fib = [0] * n 
+    if n == 0: 
+        return 0
+
+    if n == 1: 
+        return 1
+
     fib[0] = 1 
     fib[1] = 1 
 
@@ -32,6 +35,6 @@ def bottom_up_appraoch_Test(n):
 def top_down_approach_Test(n): 
     print("Top Down Approach fib({}): {}\n".format(n, locale.format("%d", top_down_fib(n), grouping=True)))
 
-bottom_up_appraoch_Test(49) 
-top_down_approach_Test(49)
+bottom_up_appraoch_Test(3) 
+top_down_approach_Test(3)
 
